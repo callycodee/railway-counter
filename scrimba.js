50b26d03f422ccc1c727213042b64842e312aa01
callycodee <149681372+callycodee@users.noreply.github.com>
2023-11-22 16:34:43 +0100

    we have added some of files

---
+++ scrimba.js
@@ -0,0 +1,18 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+// var name = window.prompt('what is your name');
+// console.console.log(4+5);
